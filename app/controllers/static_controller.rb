547d8960f4e6b3fef4cb0076061c9df788741110
Lisss13 <mironovarseniy@gmail.com>
2020-03-23 20:19:01 +0300

    add author page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,8 @@
 class StaticController < ApplicationController
   def about
   end
+
+  def author
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get '/about', to: 'static#about'
+  # get '/about', to: 'static#about'
+
+  scope 'about' do
+    get '/', to: 'static#about'
+    get '/author', to: 'static#author'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
