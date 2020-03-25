406b1c608292142729234acfa1395891243fd7fb
Lisss13 <mironovarseniy@gmail.com>
2020-03-25 16:09:19 +0300

    fix version

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,11 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module CoursesApp
+
+  def self.version
+    '0.1.0'
+  end
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
