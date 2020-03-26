12281ed7d82dfb74d7d221552008bb86c07c1229
Lisss13 <mironovarseniy@gmail.com>
2020-03-26 19:07:07 +0300

    fix static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  # get '/about', to: 'static#about'
-
-  scope 'about' do
-    get '/', to: 'static#about'
-    get '/author', to: 'static#author'
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
